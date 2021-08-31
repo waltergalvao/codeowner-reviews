e196241026ad601468816ce3991a74e7f50d7fab
Walter Barros Galvao Neto <walterbgneto@gmail.com>
2021-08-30 18:30:05 -0600

    Added logic to count CO approvals

--- dist/index.js
+++ dist/index.js
@@ -10,45 +10,41 @@ const { Octokit } = __nccwpck_require__(762);
 const { paginateRest } = __nccwpck_require__(193);
 
 async function run() {
-  try {
-    const owner = core.getInput('owner');
-    const repo = core.getInput('repo');
-    const token = core.getInput('github-token', {required: true});
-    const pullNumber = core.getInput('pull_number');
-    const codeOwners = core.getInput('codeowners', {required: true});
+  const owner = core.getInput('owner');
+  const repo = core.getInput('repo');
+  const token = core.getInput('github-token', {required: true});
+  const pullNumber = core.getInput('pull_number');
+  const codeOwners = core.getInput('codeowners', {required: true});
 
-    const octokit = Octokit.plugin(paginateRest);
-    const client = new octokit({auth: `token ${token}`});
+  const octokit = Octokit.plugin(paginateRest);
+  const client = new octokit({auth: `token ${token}`});
 
-    const results = await client.paginate(
-      `GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews`,
-      {
-        owner,
-        repo,
-        pull_number: pullNumber,
-        per_page: 100
-      }
-    );
-
-    let totalApprovals = 0;
-    const codeOwnerApprovals = 0;
-
-    core.info(`codeOwners: ${codeOwners}`);
-
-    for (const result of results) {
-      if (result.state !== 'APPROVED') continue;
-
-      totalApprovals = totalApprovals + 1;
-
-      core.info(result);
+  const results = await client.paginate(
+    `GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews`,
+    {
+      owner,
+      repo,
+      pull_number: pullNumber,
+      per_page: 100
     }
+  );
 
-    core.setOutput('approval_count', totalApprovals);
-    core.setOutput('co_approval_count', codeOwnerApprovals);
-    core.setOutput('non_co_approval_count', totalApprovals - codeOwnerApprovals);
-  } catch (error) {
-    core.setFailed(error.message);
+  let totalApprovals = 0;
+  let codeOwnerApprovals = 0;
+
+  for (const result of results) {
+    if (result.state !== 'APPROVED') continue;
+
+    totalApprovals = totalApprovals + 1;
+
+    if (codeOwners.includes(`@${result.user.login}`)) {
+      codeOwnerApprovals = codeOwnerApprovals + 1;
+    }
   }
+
+  core.setOutput('approval_count', totalApprovals);
+  core.setOutput('co_approval_count', codeOwnerApprovals);
+  core.setOutput('non_co_approval_count', totalApprovals - codeOwnerApprovals);
 }
 
 run();
